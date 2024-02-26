9e774889554fe43480a8a8c81883e3ff8cba9aca
rahul <rahul@aospa.co>
2024-02-26 14:58:44 +0000

    cupid: Build PowerShare
    Change-Id: I95a511775335f4476667b8176487e29caff39978

--- device.mk
+++ device.mk
@@ -48,6 +48,13 @@ PRODUCT_PACKAGES += \
     CupidNfcOverlay \
     CupidSystemUIOverlay
 
+# Powershare
+PRODUCT_PACKAGES += \
+    vendor.aospa.powershare-service
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.cupid.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.cupid.rc
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.xiaomi

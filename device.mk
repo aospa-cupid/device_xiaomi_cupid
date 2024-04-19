c588e7458aa06410eeb6d64a6a6ecb23b4a7f52b
Sugakesshaa <sugakesshaa@pixelos.net>
2024-04-20 02:03:22 +0800

    cupid: Define prebuilt kernel path
    Change-Id: Id0f16bfc6a7ddc3e2b644d71e95b1a428f661a63

--- device.mk
+++ device.mk
@@ -22,6 +22,9 @@ $(call inherit-product, vendor/xiaomi/cupid/cupid-vendor.mk)
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.product.mod_device=cupid_global
 
+# Kernel
+KERNEL_PREBUILT_DIR := $(LOCAL_PATH)-kernel
+
 # Overlays
 PRODUCT_PACKAGES += \
     AOSPACupidFrameworksOverlay \

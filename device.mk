1e0e2edfa94bb2b5003a5b00735ec5b5ce8224d9
rahul <rahul@aospa.co>
2024-08-01 13:33:54 +0000

    Revert "cupid: configs/display: Slightly adjust values in displayconfig"
    This reverts commit 5ebac2f05922224f6780c2e7b36f6f9709679430.

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 # Display - config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/display/display_id_4630946545580055170.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946545580055170.xml
+    $(DEVICE_PATH)/configs/displayconfig/display_id_4630946545580055170.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946545580055170.xml
 
 # Display - timers
 PRODUCT_ODM_PROPERTIES += \
